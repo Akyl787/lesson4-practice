a976c12047bcaa67aec78b9ca82ec285ff0dc474
Akyl <kurmanalievakyl5@gmail.com>
2023-04-05 15:34:25 +0600

    lesson 4 practice if else

---
+++ lesson4(practice).dart
@@ -0,0 +1,160 @@
+void main() {
+  int a = 10;
+  if(a == 10){
+    print("Верно");
+  } else{
+    print("Неверно");
+  }
+
+  int b = 0;
+  if(b == 0){
+    print("Верно");
+  } else{
+    print("Неверно");
+  }
+  int b1 = 1;
+  if(b1 == 1){
+    print("Верно");
+  } else{
+    print("Неверно");
+  }
+  int b2 = -3;
+  if(b1 == -1){
+    print("Верно");
+  } else{
+    print("Неверно");
+  }
+
+  int c = 1;
+  if(c > 0){
+    print("Верно");
+  } else{
+    print("Неверно");
+  }
+  int c1 = 0;
+  if(c1 > 0){
+    print("Верно");
+  } else{
+    print("Неверно");
+  }
+  int c2 = -3;
+  if(c2 > 0){
+    print("Верно");
+  } else{
+    print("Неверно");
+  }
+
+  int d = -1;
+  if(d < 0){
+    print("Верно");
+  } else{
+    print("Неверно");
+  }
+  int d1 = 0;
+  if(d1 < 0){
+    print("Верно");
+  } else{
+    print("Неверно");
+  }
+  int d2 = -3;
+  if(d2 < 0){
+    print("Верно");
+  } else{
+    print("Неверно");
+  }
+
+  int f = 1;
+  if(f >= 0){
+    print("Верно");
+  } else{
+    print("Неверно");
+  }
+  int f1 = 0;
+  if(f >= 0){
+    print("Верно");
+  } else{
+    print("Неверно");
+  }
+  int f2 = -3;
+  if(f2 >= 0){
+    print("Верно");
+  } else{
+    print("Неверно");
+  }
+
+  int g = 1;
+  if(g <= 0){
+    print("Верно");
+  } else{
+    print("Неверно");
+  }
+  int g1 = 0;
+  if(g1 <= 0){
+    print("Верно");
+  } else{
+    print("Неверно");
+  }
+  int g2 = -3;
+  if(g2 <= 0){
+    print("Верно");
+  } else{
+    print("Неверно");
+  }
+
+  int h = 1;
+  if(h != 0){
+    print("Верно");
+  }else{
+    print("Неверно");
+  }
+  int h1 = 0;
+  if(h1 != 0){
+    print("Верно");
+  }else{
+    print("Неверно");
+  }
+  int h2 = -3;
+  if(h2 != 0){
+    print("Верно");
+  }else{
+    print("Неверно");
+  }
+
+  String k = "test";
+  if(k == "test"){
+    print("Верно");
+  }else{
+    print("Неверно");
+  }
+  String k1 = "test";
+  if(k1 == "тест"){
+    print("Верно");
+  }else{
+    print("Неверно");
+  }
+  String k2 = "test";
+  if(k2 == 3){
+    print("Верно");
+  }else{
+    print("Неверно");
+  }
+
+  String m = "1";
+  if(m == "1"){
+    print("Верно");
+  }else{
+    print("Неверно");
+  }
+  int m1 = 1;
+  if(m1 == 1){
+    print("Верно");
+  }else{
+    print("Неверно");
+  }
+  int m2 = 3;
+  if(m2 == 3){
+    print("Верно");
+  }else{
+    print("Неверно");
+  }
+}
